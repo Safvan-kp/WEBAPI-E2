1cc6f21a52e5ffaeef331a0ac841c50e1d816832
Safvan-kp <safvan.kp@gadgeon.com>
2023-05-18 11:17:58 +0530

    First change

---
+++ WEBAPI-E2/Models/RegistrationModel.cs
@@ -0,0 +1,11 @@
+﻿namespace WEBAPI_E2.Models
+{
+    public class RegistrationModel
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int IsActive { get; set; }
+    }
+}
---
+++ WEBAPI-E2/Models/ResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WEBAPI_E2.Models
+{
+    public class ResponseModel
+    {
+        public int statusCode { get; set; }
+        public string statusMessage { get; set; }
+    }
+}
