304a9890aa805476920ab849917973182540fa2a
Safvan-kp <safvan.kp@gadgeon.com>
2023-05-23 16:20:30 +0530

    First

---
+++ WEBAPI-E2/Data/WEBAPI_E2DbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using WEBAPI_E2.Models;
+
+namespace WEBAPI_E2.Data
+{
+    public class WEBAPI_E2DbContext : DbContext
+    {
+        public WEBAPI_E2DbContext(DbContextOptions options) : base(options)
+        {
+
+
+        }
+        public DbSet<StockModel> Stocks { get; set; }
+    }
+}
--- WEBAPI-E2/Program.cs
+++ WEBAPI-E2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using WEBAPI_E2.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<WEBAPI_E2DbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("LearnDB")));
 
 var app = builder.Build();
 
